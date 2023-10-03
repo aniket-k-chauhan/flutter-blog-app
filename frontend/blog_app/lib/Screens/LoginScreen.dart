b8decebe7077f79e29cf7d780137c9e39a1801b0
Aniket Chauhan <chauhananiket12801@gmail.com>
2023-10-03 19:21:18 +0530

    implement Register and Login Screens.

---
+++ frontend/blog_app/lib/Screens/LoginScreen.dart
@@ -0,0 +1,126 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              RegisterForm(),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "New user? ",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({super.key});
+
+  @override
+  State<RegisterForm> createState() {
+    return _RegisterFormState();
+  }
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          inputField(context, "Email", false, emailController),
+          inputField(context, "Password", true, pwdController),
+          Container(
+            width: MediaQuery.of(context).size.width - 90,
+            height: 45,
+            margin: const EdgeInsets.symmetric(vertical: 16),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Login",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container inputField(BuildContext context, String labelText, bool obscureText,
+      TextEditingController controller) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 70,
+      height: 55,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        style: TextStyle(fontSize: 17),
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: TextStyle(
+            fontSize: 17,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              width: 1.5,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: Colors.grey,
+              width: 1,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/blog_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:blog_app/Screens/LoginScreen.dart';
+import 'package:blog_app/Screens/RegisterScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
