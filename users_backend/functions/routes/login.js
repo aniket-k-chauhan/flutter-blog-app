f32b5b19b4a81206c5973a55718b9d642d49fb92
Aniket Chauhan <chauhananiket12801@gmail.com>
2023-10-08 01:35:58 +0530

    remove temporary user_backend folder.

--- users_backend/functions/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res, next) => {
-    // logic to login user
-});
-
-module.exports = router
--- users_backend/functions/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res, next) => {
-    // register to login user
-});
-
-module.exports = router
